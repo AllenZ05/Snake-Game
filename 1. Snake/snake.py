8e86d539a0e9b509ca82c24983036bd9d2fefc3f
Allen Zhang <allen.zhang.y05@gmail.com>
2023-10-24 23:45:21 -0400

    Done snake

--- 1. Snake/snake.py
+++ 1. Snake/snake.py
@@ -11,17 +11,33 @@ class FRUIT:
         self.pos = Vector2(self.x, self.y)
 
     def draw_fruit(self):
-        fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
+        x_fruit = int(self.pos.x * cell_size)
+        y_fruit = int(self.pos.y * cell_size)
+        fruit_rect = pygame.Rect(x_fruit, y_fruit, cell_size, cell_size)
         pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
+
+
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+
+    def draw_snake(self):
+        for block in self.body:
+            x_snake = int(block.x * cell_size)
+            y_snake = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_snake, y_snake, cell_size, cell_size)
+            pygame.draw.rect(screen, (183, 111, 122), block_rect)
 
 
 pygame.init()
 cell_size = 40
 cell_number = 20
-screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))  
-clock = pygame.time.Clock()  
+screen = pygame.display.set_mode(
+    (cell_number * cell_size, cell_number * cell_size))
+clock = pygame.time.Clock()
 
 fruit = FRUIT()
+snake = SNAKE()
 
 while True:
     for event in pygame.event.get():
@@ -29,7 +45,8 @@ while True:
             pygame.quit()
             sys.exit()
 
-    screen.fill((175, 220, 75))  
+    screen.fill((175, 220, 75))
     fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
-    clock.tick(60) 
+    clock.tick(60)
