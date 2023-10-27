6573aec41a2172160fe7d8c507e7e1c7bf9f3fce
Allen Zhang <allen.zhang.y05@gmail.com>
2023-10-26 21:59:13 -0400

    Added grass pattern

--- 1. Snake/snake.py
+++ 1. Snake/snake.py
@@ -56,11 +56,12 @@ class MAIN:
     def update(self):
         self.snake.move_snake()
         self.check_collision()
+        self.check_fail()
 
     def draw_elements(self):
+        self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
-        self.check_fail()
 
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
@@ -78,6 +79,20 @@ class MAIN:
     def game_over(self):
         pygame.quit()
         sys.exit()
+
+    def draw_grass(self):
+        grass_color = (167, 209, 61)
+        for row in range(cell_number):
+            if row % 2 == 0:
+                for col in range(cell_number):
+                    if col % 2 == 0:
+                        grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+            else:
+                for col in range(cell_number):
+                    if col % 2 != 0:
+                        grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
 
 
 pygame.init()
