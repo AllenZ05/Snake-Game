bbfc3d872faa0e038fc43adb419665fc82583606
Allen Zhang <allen.zhang.y05@gmail.com>
2023-10-28 03:51:54 -0400

    Redesign the main parts of the snake body

--- 1. Snake/snake.py
+++ 1. Snake/snake.py
@@ -57,8 +57,14 @@ class SNAKE:
                 screen.blit(self.head, block_rect)
             elif index == len(self.body) - 1: 
                 screen.blit(self.tail, block_rect)
-            else:
-                pygame.draw.rect(screen, (150, 100, 100), block_rect)
+            else: 
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal, block_rect)
+                
     
     def update_head_graphics(self):
         head_relation = self.body[1] - self.body[0]
